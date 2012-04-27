acf35ec5da43976fb3dd9b7a58f18ad8e61c6d56
adamz667 <adams@g.pl>
2012-04-27 16:01:38 +0300

    Leave the package overlays for now

--- device-vendor.mk
+++ device-vendor.mk
@@ -27,6 +27,6 @@ PRODUCT_COPY_FILES := \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
 # Pick up overlay for features that depend on non-open-source files
-DEVICE_PACKAGE_OVERLAYS := vendor/htc/vision/overlay
+# DEVICE_PACKAGE_OVERLAYS := vendor/htc/vision/overlay
 
 $(call inherit-product, vendor/htc/vision/vision-vendor-blobs.mk)
